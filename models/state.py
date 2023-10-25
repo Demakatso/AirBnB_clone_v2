62cf6c62284c853aa21fbdc53a876db6cba53d32
Demakatso <demakatsomay@gmail.com>
2023-10-25 13:10:46 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -3,26 +3,30 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, Integer, String
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-
-# Check the type of storage (db or file)
-storage_type = models.storage.__class__.__name__
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    if storage_type == 'DBStorage':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete')
-    else:
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    def __init__(self, *args, **kwargs):
+        """ init state """
+        super().__init__(*args, **kwargs)
+
+    if models.storage_type != "db":
         @property
         def cities(self):
-            """getter for list of city instances"""
-            cities = models.storage.all(City)
-            return [city for city in cities.values()
-                    if city.state_id == self.id]
+            """ list cities """
+            list_city = []
+            all_inst_c = models.storage.all(City)
+            for value in all_inst_c.values():
+                if value.state_id == self.id:
+                    list_city.append(value)
+            return list_city
